a1ee646e4c2b8abafae425d5509b8a573a1f0b1f
MarbleToast <40271160+MarbleToast@users.noreply.github.com>
2020-03-09 17:45:09 +0000

    Test App comments

--- beanbags/src/JarProcessTestApp.java
+++ beanbags/src/JarProcessTestApp.java
@@ -26,50 +26,69 @@ import beanbags.Store;
  */
 public class JarProcessTestApp {
 	public static void main(String[] args) {
-		BeanBagStore badStore = new BadStore();
-		System.out.println("BadStore instance successfully made, with "
-                           + badStore.beanBagsInStock()
-                           + " beanbags in stock.");
 			
+		// Create Store instance.
 		Store goodStore = new Store();
 		System.out.println("Store instance successfully made, with "
                 + goodStore.beanBagsInStock()
                 + " beanbags in stock.");
+		
+		/*
+		 * Test: Add 10 beanbags to stock.
+		 * Expected result: Successfully added.
+		 * Result: Successfully added.
+		 */
+		System.out.println("Adding 10 beanbags.");
 		try {
 			goodStore.addBeanBags(10, "beans", "bug", "12345678",(short) 2020, (byte) 5);
+			System.out.println("Successfully added.");
 		} catch (IllegalNumberOfBeanBagsAddedException | BeanBagMismatchException | IllegalIDException
 				| InvalidMonthException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		/*
+		 * Test: Attempt to 10 beanbags to stock with the same ID but differing text.
+		 * Expected result: BeanbagMismatchException.
+		 * Result: 
+		   beanbags.BeanBagMismatchException
+			   at beanbags.Store.addBeanBags(Store.java:71)
+		       at JarProcessTestApp.main(JarProcessTestApp.java:56)
+		 */
+		System.out.println("Adding 10 beanbags.");
 		try {
 			goodStore.addBeanBags(10, "bean", "bug", "12345678",(short) 2020, (byte) 5);
+			System.out.println("Successfully added.");
 		} catch (IllegalNumberOfBeanBagsAddedException | BeanBagMismatchException | IllegalIDException
 				| InvalidMonthException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("Store instance successfully made, with "
-                + goodStore.beanBagsInStock()
-                + " beanbags in stock.");
-		try {
-			goodStore.setBeanBagPrice("12345678", 100);
-		} catch (InvalidPriceException | BeanBagIDNotRecognisedException | IllegalIDException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		
+		System.out.println("Store instance currently holds " + goodStore.beanBagsInStock() + " beanbags in stock.");
+		
 		try {
 			System.out.print(goodStore.reserveBeanBags(9, "12345678"));
 		} catch (BeanBagNotInStockException | InsufficientStockException | IllegalNumberOfBeanBagsReservedException
 				| PriceNotSetException | BeanBagIDNotRecognisedException | IllegalIDException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			goodStore.setBeanBagPrice("12345678", 100);
+		} catch (InvalidPriceException | BeanBagIDNotRecognisedException | IllegalIDException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			System.out.print(goodStore.reserveBeanBags(9, "12345678"));
+		} catch (BeanBagNotInStockException | InsufficientStockException | IllegalNumberOfBeanBagsReservedException
+				| PriceNotSetException | BeanBagIDNotRecognisedException | IllegalIDException e) {
 			e.printStackTrace();
 		}
 		try {
 			goodStore.sellBeanBags(1, "12345678");
 		} catch (BeanBagNotInStockException | InsufficientStockException | IllegalNumberOfBeanBagsSoldException
 				| PriceNotSetException | BeanBagIDNotRecognisedException | IllegalIDException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
